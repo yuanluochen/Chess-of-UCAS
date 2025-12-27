7f4d4109b48025471979acccd758dd9c051b0878
yuanluochen <yuanluochen@foxmail.com>
2025-12-27 22:57:11 +0800

    修改一下代码

--- source/main.cpp
+++ source/main.cpp
@@ -3,20 +3,6 @@
 #include "chessGame.hpp"
 #include "decisionTree.hpp"
 using namespace std;
-void man_machine(chessGame & game) {
-  while (1){
-    cout << game << endl;
-    std::cout << "player chess:=" << std::endl;
-    char row, col;
-    cin >> row;
-    cin >> col;
-    game.setChessPiece(row - '0', col - 'a', WRITE);
-    cout << game << endl;
-    DecisionTree ai(game, 3, BLACK); // 深度为3，AI执黑
-    auto bestMove = ai.getBestMove();
-    game.setChessPiece(bestMove.first, bestMove.second, BLACK);
-  }
-}
 
 int main(){
   chessGame game(15); // 创建15x15的棋盘
